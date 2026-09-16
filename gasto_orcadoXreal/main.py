58fbbdae9466515138ec73ca190c7c42fba07cb9
Kauã Rodrigues <kauargt05@gmail.com>
2026-09-16 15:27:41 -0300

    feat:função para plotar o grafico

--- gasto_orcadoXreal/main.py
+++ gasto_orcadoXreal/main.py
@@ -1,28 +1,49 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-dados = {
-    "Categoria": ["Alimentação","Transporte","Lazer","Moradia","Saúde"],
-    "Gasto Orcado": [800,300,200,1200,150],
-    "Gasto Real": [920,280,350,1200,100]
+#Função para Plotar Informações
+
+def plotar(cat, orc, rea):
+  dados = {
+    "Categoria": cat,
+    "Gasto Orcado": orc,
+    "Gasto Real": rea
     }
 
-df = pd.DataFrame(dados)
+  df = pd.DataFrame(dados)
 
-ax = df.plot(
-  x="Categoria",
-  y=["Gasto Orcado", "Gasto Real"],
-  kind="bar",
-  figsize=(9,5),
-  color=["#4C72B0","#DD8452"]
-)
+  ax = df.plot(
+    x="Categoria",
+    y=["Gasto Orcado", "Gasto Real"],
+    kind="bar",
+    figsize=(9,5),
+    color=["#4C72B0","#DD8452"]
+  )
 
-plt.title("Comprativo: Orçado Vs Realizado (R$)", fontsize=13, fontweight="bold")
-plt.xlabel("Categora de Despesa", fontsize=11)
-plt.ylabel("Valor em R$", fontsize=11)
-plt.xticks(rotation=0)
-plt.grid(axis="y", linestyle="--", alpha=0.5)
-plt.legend(["Orçado", "Realizado"])
+  plt.title("Comprativo: Orçado Vs Realizado (R$)", fontsize=13, fontweight="bold")
+  plt.xlabel("Categora de Despesa", fontsize=11)
+  plt.ylabel("Valor em R$", fontsize=11)
+  plt.xticks(rotation=0)
+  plt.grid(axis="y", linestyle="--", alpha=0.5)
+  plt.legend(["Orçado", "Realizado"])
 
-plt.tight_layout()
-plt.show()
+  plt.tight_layout()
+  return plt.show()
+
+#Código principal
+
+print("Calcular Orçamento X Realizado")
+print("Como Usar:")
+print("Defina categorias, gastos orçados e por ultimo gastos realizados")
+c = int(input("Quantas Categorias deseja adicionar? (Númerico)"))
+categorias = []
+orcado = []
+realizado = []
+for cat in range(0,c):
+ categorias.append(input(f"Qual o nome da {cat+1}° categoria? "))
+ orcado.append(float(input(f"Qual o valor orçado para a categoria: {categorias[cat]} ? ")))
+ realizado.append(float(input(f"Qual o valor Gasto para a categoria: {categorias[cat]} ? ")))
+
+plotar(categorias, orcado, realizado)
+
+
