ab9912b98d61a04ff297df54f9f56b9ffcb167c6
Kauã Rodrigues <kauargt05@gmail.com>
2026-09-16 16:58:32 -0300

    feat: Criação da função gerarGraficos() e modularização das funções e arquivos

--- gasto_orcadoXreal/main.py
+++ gasto_orcadoXreal/main.py
@@ -1,49 +1,7 @@
-import pandas as pd
-import matplotlib.pyplot as plt
+#importa todas as funções do arquivo funcoes.py
+import funcoes
 
-#Função para Plotar Informações
-
-def plotar(cat, orc, rea):
-  dados = {
-    "Categoria": cat,
-    "Gasto Orcado": orc,
-    "Gasto Real": rea
-    }
-
-  df = pd.DataFrame(dados)
-
-  ax = df.plot(
-    x="Categoria",
-    y=["Gasto Orcado", "Gasto Real"],
-    kind="bar",
-    figsize=(9,5),
-    color=["#4C72B0","#DD8452"]
-  )
-
-  plt.title("Comprativo: Orçado Vs Realizado (R$)", fontsize=13, fontweight="bold")
-  plt.xlabel("Categora de Despesa", fontsize=11)
-  plt.ylabel("Valor em R$", fontsize=11)
-  plt.xticks(rotation=0)
-  plt.grid(axis="y", linestyle="--", alpha=0.5)
-  plt.legend(["Orçado", "Realizado"])
-
-  plt.tight_layout()
-  return plt.show()
-
-#Código principal
-
-print("Calcular Orçamento X Realizado")
-print("Como Usar:")
-print("Defina categorias, gastos orçados e por ultimo gastos realizados")
-c = int(input("Quantas Categorias deseja adicionar? (Númerico)"))
-categorias = []
-orcado = []
-realizado = []
-for cat in range(0,c):
- categorias.append(input(f"Qual o nome da {cat+1}° categoria? "))
- orcado.append(float(input(f"Qual o valor orçado para a categoria: {categorias[cat]} ? ")))
- realizado.append(float(input(f"Qual o valor Gasto para a categoria: {categorias[cat]} ? ")))
-
-plotar(categorias, orcado, realizado)
+#executa a função GerarGraficos que por sua vez executa as funções lerDados() e plotar().
+funcoes.GerarGraficos()
 
 
